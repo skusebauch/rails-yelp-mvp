93427d355d52b75dced6fe5da176ee2ad6b7314a
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-05-20 15:40:45 +0200

    added review feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/restaurants', to: 'restaurants#index'
-  get '/restaurants/new', to: 'restaurants#new'
-  post '/restaurants', to: 'restaurants#create'
-  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    # get '/restaurants', to: 'restaurants#index'
+    # get '/restaurants/new', to: 'restaurants#new'
+    # post '/restaurants', to: 'restaurants#create'
+    # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+    resources :reviews, only: [:new, :create]
+  end
 end
