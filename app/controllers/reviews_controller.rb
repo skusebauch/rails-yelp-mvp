4a411fb4ef4d2f64269d92d568eac425aee82092
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-05-20 16:01:39 +0200

    refactor #set_review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,11 @@
 class ReviewsController < ApplicationController
+  before_action :set_review, only: [:new, :create]
+
   def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
     if @review.save
@@ -19,6 +19,10 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:content, :rating)
+  end
+
+  def set_review
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 
 end
